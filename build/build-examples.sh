601ba092b231d3f5fa296aabda6e55c36069df6e
marc <marc@cockroachlabs.com>
2016-03-23 14:01:28 -0400

    make static builds work again without docker
    * local go install (the netgo suffix causes permission denied)
    * don't check for "statically linked", they aren't.
    * also strip photos binary

--- build/build-examples.sh
+++ build/build-examples.sh
@@ -3,14 +3,11 @@
 #
 # Author: Peter Mattis (peter@cockroachlabs.com)
 
-set -euo pipefail
+set -eux
 
 time make deps
 time make STATIC=1 block_writer
 time make STATIC=1 photos
 
-# Make sure the created binary is statically linked.  Seems
-# awkward to do this programmatically, but this should work.
-file block_writer/block_writer | grep -F 'statically linked' > /dev/null
-
 strip -S block_writer/block_writer
+strip -S photos/photos

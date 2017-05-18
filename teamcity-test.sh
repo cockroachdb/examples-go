ded4934f387b3c9adc1b97965427f5467027ec4d
Tamir Duberstein <tamird@gmail.com>
2017-05-18 10:03:09 -0400

    fixup

--- teamcity-test.sh
+++ teamcity-test.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 docker run \
 	--workdir=/go/src/github.com/cockroachdb/examples-go \
-	--volume="${GOPATH%%:*}/src":/go/src \
+	--volume="$(dirname "${0}")":/go/src/github.com/cockroachdb/examples-go \
 	--rm \
 	cockroachdb/builder:20170422-212842 make deps build test | go-test-teamcity
